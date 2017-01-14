89da7aa0e31720061bce06187cfd3f91b7591024
Michael R Creel <michaelrcreel@gmail.com>
2017-01-14 11:38:29 -0600

    initial commit

--- attendance.R
+++ attendance.R
@@ -1,4 +1,11 @@
-source("source.R")
+library("tidyverse")
+
+# By http://stackoverflow.com/users/2572423/jasonaizkalns
+# From http://stackoverflow.com/a/33525389/6269439
+is_outlier <- function(x) {
+    return(x < quantile(x, 0.25) - 1.5 * IQR(x) | x > quantile(x, 0.75) + 1.5 * IQR(x))
+}
+
 
 team <- read.csv("./data/team.csv")
 park <- read_csv("./data/park.csv")
@@ -9,7 +16,7 @@ tbl_team <- team %>%
            park != "") %>%
     select(year:ws_win,
            name:attendance) %>%
-    mutate(att_game = as.integer(attendance / ghome))
+    mutate(avg_attendance = as.integer(attendance / ghome))
 
 tbl_park <- park %>%
     select(park_id, park_name)
@@ -17,9 +24,9 @@ tbl_park <- park %>%
 tbl_team %>% 
     group_by(park) %>% 
     mutate(n=n()) %>%
-    mutate(outlier = ifelse(is_outlier(att_game), year, as.numeric(NA))) %>%
+    mutate(outlier = ifelse(is_outlier(avg_attendance), year, as.numeric(NA))) %>%
     filter(year>1969, n >2) %>%
-    ggplot(., aes(x = factor(park), y = att_game)) +
+    ggplot(., aes(x = factor(park), y = avg_attendance)) +
     geom_boxplot() + 
     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
     geom_text(aes(label = outlier), na.rm = TRUE, size = 3, hjust = -0.1)
